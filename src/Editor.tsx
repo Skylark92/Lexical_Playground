eb05d48626b878f6274cdee23ecd4680c2081180
Skylark92 <skylark92@live.co.kr>
2024-01-18 21:49:45 +0900

    onChange와 BLUR_COMMAND 동작 확인

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,12 +1,10 @@
-import { $getRoot, $getSelection, EditorState } from "lexical";
+import { BLUR_COMMAND, COMMAND_PRIORITY_NORMAL } from "lexical";
 import { useEffect, useRef } from "react";
 
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
@@ -15,10 +13,6 @@ const theme = {
   // ...
 };
 
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
 function MyCustomAutoFocusPlugin() {
   const [editor] = useLexicalComposerContext();
 
@@ -30,23 +24,37 @@ function MyCustomAutoFocusPlugin() {
   return null;
 }
 
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
 function onError(error: Error) {
   console.error(error);
 }
 
-function Save() {
+function CustomOnChangePlugin() {
   const [editor] = useLexicalComposerContext();
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+
+  useEffect(() => {
+    return editor.registerCommand(
+      BLUR_COMMAND,
+      () => {
+        if (timer.current) clearTimeout(timer.current);
+        console.log("blur!!!");
+        return false;
+      },
+      COMMAND_PRIORITY_NORMAL
+    );
+  }, [editor]);
 
   useEffect(() => {
     const removeUpdateListener = editor.registerUpdateListener(
       ({ editorState }) => {
-        console.log(editorState);
+        console.log("timer in onchange: ", timer.current);
+        if (timer.current) clearTimeout(timer.current);
+        timer.current = setTimeout(() => {
+          console.log("onChange!!!");
+        }, 3000);
       }
     );
-    return removeUpdateListener();
+    // return removeUpdateListener();
   }, [editor]);
 
   return null;
@@ -58,11 +66,6 @@ function Editor() {
     theme,
     onError,
   };
-  const editorStateRef = useRef<EditorState>();
-
-  useEffect(() => {
-    console.log(editorStateRef.current?.toJSON());
-  }, [editorStateRef.current]);
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
@@ -71,12 +74,9 @@ function Editor() {
         placeholder={<div>Enter some text...</div>}
         ErrorBoundary={LexicalErrorBoundary}
       />
-      <OnChangePlugin
-        onChange={(editorState) => (editorStateRef.current = editorState)}
-      />
       <HistoryPlugin />
       <MyCustomAutoFocusPlugin />
-      <Save />
+      <CustomOnChangePlugin />
     </LexicalComposer>
   );
 }
